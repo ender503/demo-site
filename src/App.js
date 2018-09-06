06992a2a8a8d4135167891e75909ae5b33ea3ad9
ender503 <ender503.tw@gmail.com>
2018-09-07 01:09:50 +0800

    Add react-router for navigate between pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
 
+import HomePage from './containers/HomePage';
+import AdminPage from './containers/AdminPage';
 class App extends React.Component {
   render() {
     return (
-      <div></div>
+      <Router>
+        <div>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/admin">Manager</Link></li>
+          </ul>
+
+          <hr/>
+
+          <Route exact path="/" component={HomePage}/>
+          <Route path="/admin" component={AdminPage}/>
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/containers/AdminPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class AdminPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>AdminPage</p>
+      </div>
+    )
+  }
+}
+
+export default AdminPage;
