d0261890e81208ca029e2cd208132633a719d28c
ender503 <ender503.tw@gmail.com>
2018-10-12 15:28:27 +0800

    Drop unused AdminPage

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Header from './components/Header';
 
 import HomePage from './containers/HomePage';
-import AdminPage from './containers/AdminPage';
 class App extends React.Component {
   render() {
     return (
@@ -14,7 +13,6 @@ class App extends React.Component {
           <Header />
           <Container>
             <Route exact path="/" component={HomePage} />
-            <Route path="/admin" component={AdminPage} />
           </Container>
         </div>
       </Router>
