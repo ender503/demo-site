88381ba16a2e72a8c46da198890186168ac721fa
ender503 <ender503.tw@gmail.com>
2018-10-12 15:59:17 +0800

    Redraw Header component
    Since the Header component doesn't need Navigation Bar, redraw the
    Header component with customizing style sheets.
    
    To use customize style sheets with SCSS, install the node-sass package
    and react-scripts will automate load it.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,67 +1,17 @@
 import React from 'react';
-import {
-  Collapse,
-  DropdownItem,
-  DropdownMenu,
-  DropdownToggle,
-  Nav,
-  NavItem,
-  Navbar,
-  NavbarBrand,
-  NavbarToggler,
-  UncontrolledDropdown
-} from 'reactstrap';
 
 import { Link } from 'react-router-dom';
 
 class Header extends React.Component {
-  constructor(props) {
-    super(props);
-    this.toggleNavbar = this.toggleNavbar.bind(this);
-    this.state = {
-      collapsed: true
-    };
-  }
-
-  toggleNavbar() {
-    this.setState({
-      collapsed: !this.state.collapsed
-    });
-  }
-
   render() {
     return (
-      <Navbar color="dark" dark expand="md">
-        <NavbarBrand href="/" className="mr-auto">
-          reactstrap
-        </NavbarBrand>
-        <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
-        <Collapse isOpen={!this.state.collapsed} navbar>
-          <Nav className="ml-auto" navbar>
-            <NavItem>
-              <Link className="nav-link" to="/">
-                User
-              </Link>
-            </NavItem>
-            <NavItem>
-              <Link className="nav-link" to="/admin">
-                Manager
-              </Link>
-            </NavItem>
-            <UncontrolledDropdown nav inNavbar>
-              <DropdownToggle nav caret>
-                Options
-              </DropdownToggle>
-              <DropdownMenu right>
-                <DropdownItem>Option 1</DropdownItem>
-                <DropdownItem>Option 2</DropdownItem>
-                <DropdownItem divider />
-                <DropdownItem>Reset</DropdownItem>
-              </DropdownMenu>
-            </UncontrolledDropdown>
-          </Nav>
-        </Collapse>
-      </Navbar>
+      <header className="header">
+        <div className="container">
+          <Link className="main-logo" to="/">
+            TangleID Demo | Tangle
+          </Link>
+        </div>
+      </header>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './stylesheets/main.scss';
 import registerServiceWorker from './registerServiceWorker';
 
 if (process.env.NODE_ENV === 'development' && module.hot) {
