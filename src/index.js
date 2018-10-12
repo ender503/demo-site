f4117c305a6b4af8b72282970419dc2893b65364
ender503 <ender503.tw@gmail.com>
2018-10-12 15:05:27 +0800

    Enable hot reload in development mode
    To speed up the website reload when code changes, enable the hot module
    replace(HMR) in development mode.

--- src/index.js
+++ src/index.js
@@ -4,5 +4,9 @@ import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import registerServiceWorker from './registerServiceWorker';
 
+if (process.env.NODE_ENV === 'development' && module.hot) {
+  module.hot.accept();
+}
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
